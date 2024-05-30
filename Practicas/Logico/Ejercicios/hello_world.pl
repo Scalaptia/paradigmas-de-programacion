ec06aebfd09a7ef651484cf622545ddcf8c228cc
Scalaptia <fernando@haroware.com>
2024-05-30 00:26:24 -0700

    Paradigma lógico (prolog)

---
+++ Practicas/Logico/Ejercicios/hello_world.pl
@@ -0,0 +1,20 @@
+GNU Prolog 1.4.5 (64 bits)
+Compiled Feb 23 2020, 20:14:50 with gcc
+By Daniel Diaz
+Copyright (C) 1999-2020 Daniel Diaz
+uncaught exception: error(existence_error(procedure,main/0),top_level/0)
+uncaught exception: error(existence_error(procedure,help/0),top_level/0)
+
+Prolog interruption (h for help) ? 
+   a  abort        b  break
+   c  continue     e  exit
+   d  debug        t  trace
+  h/? help
+
+Prolog interruption (h for help) ? 
+   a  abort        b  break
+   c  continue     e  exit
+   d  debug        t  trace
+  h/? help
+
+Prolog interruption (h for help) ? 
